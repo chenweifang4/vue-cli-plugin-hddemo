0b9ab17c845b73dc54b9184fd761fe4272f09652
chenweifang <chenweifang@yy.com>
2019-03-07 23:31:51 +0800

    feat: 添加说明

--- scripts/verifyCommitMsg.js
+++ scripts/verifyCommitMsg.js
@@ -1,8 +1,10 @@
 /**
  * Shout out to https://github.com/vuejs/vue-cli/blob/dev/scripts/verifyCommitMsg.js
  */
-
 const chalk = require('chalk')  // eslint-disable-line
+
+// process.env.GIT_PARAMS => process.env.HUSKY_GIT_PARAMS
+// https://github.com/typicode/husky/issues/113#issuecomment-407338401
 const msgPath = process.env.HUSKY_GIT_PARAMS
 const msg = require('fs').readFileSync(msgPath, 'utf-8').trim()
 
