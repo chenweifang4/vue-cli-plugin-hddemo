97d7b1266089a9d83daee5f37be4112b581c8446
chenweifang <chenweifang@yy.com>
2019-03-08 22:29:35 +0800

    release: v2.2.0

--- generator/templates/src/views/login/Login.vue
+++ generator/templates/src/views/login/Login.vue
@@ -1,17 +1,38 @@
 <template>
   <div :class="$style.Login">
-    <dv-button class="mgt10" type="primary" @click="goToLoginIndex">登录</dv-button>
+    <div :class="$style.LoginContainer">
+      <div :class="$style.LoginContainerLogo">
+        <img :src="logoIcon" alt="系统图标">
+      </div>
+      <dv-input :class="$style.LoginContainerInput" placeholder="请输入账号" v-model="params.username" @keyup.enter="handleLogin"></dv-input>
+      <dv-input type="password" :class="$style.LoginContainerInput" placeholder="请输入密码" v-model="params.password" @keyup.enter="handleLogin"></dv-input>
+      <dv-button :class="$style.LoginContainerButton" type="primary" @click="handleLogin" :loading="loading">登录</dv-button>
+    </div>
   </div>
 </template>
 <script>
+import logoIcon from '@/assets/logo.svg'
 export default {
   name: 'Login',
+  components: {},
+  props: {},
   data () {
-    return {}
+    return {
+      logoIcon,
+      loading: false,
+      params: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  computed: {
+  },
+  created () {
   },
   methods: {
-    goToLoginIndex () {
-      this.$message.success('登录成功!!')
+    async handleLogin () {
+      this.$message.success('登录成功')
       this.$router.push('/')
     }
   }
